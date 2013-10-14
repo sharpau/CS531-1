656dbf3e82d6ac081b45bca59cd655f8612a93dc
Austin Sharp <austinsharp111@gmail.com>
2013-10-13 22:22:25 -0700

    changes to record data

--- CS531-1.cpp
+++ CS531-1.cpp
@@ -21,6 +21,11 @@ int _tmain(int argc, _TCHAR* argv[])
 		progress.push_back(room.get_clean_squares());
 	}
 
+	std::ofstream reflex_csv("reflex.csv");
+	for(int i = 0; i < progress.size(); i++) {
+		reflex_csv << progress[i] << '\n';
+	}
+
 	// get a sampling of RandomAgent results
 	std::vector<int> samples[50];
 	for(int i = 0; i < 50; i++) {
@@ -36,11 +41,43 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 		samples[i] = rand_progress;
 	}
-	double avg_clean = 0;
+
+	// get average of 50 samples
+	int min_size = INT_MAX;
 	for(auto vec : samples) {
-		avg_clean += vec[vec.size() - 1];
+		if(vec.size() < min_size) {
+			min_size = vec.size();
+		}
 	}
-	avg_clean /= 50;
+	std::vector<double> avg_clean(min_size, 0);
+	for(auto vec: samples) {
+		for(int i = 0; i < min_size; i++) {
+			avg_clean[i] += vec[i];
+		}
+	}
+	for(int i = 0; i < min_size; i++) {
+		avg_clean[i] /= 50;
+	}
+
+	std::ofstream random_csv("random.csv");
+	for(int i = 0; i < avg_clean.size(); i++) {
+		random_csv << avg_clean[i] << '\n';
+	}
+
+	// find how long each trial took to get to 90% clean
+	std::vector<int> ninety_percent(50, 0);
+	for(int i = 0; i < 50; i++) {
+		for(int j = 0; j < samples[i].size(); j++) {
+			if(samples[i][j] == 90) {
+				ninety_percent[i] = j;
+				j = samples[i].size(); // break out of loop
+			}
+		}
+	}
+	std::ofstream ninety_csv("ninety.csv");
+	for(int i = 0; i < ninety_percent.size(); i++) {
+		ninety_csv << ninety_percent[i] << '\n';
+	}
 
 	room = Room(1.0f);
 	ModelAgent model(room);
@@ -51,6 +88,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	while(model.act()) {
 		// track progress
 		model_progress.push_back(room.get_clean_squares());
+	}
+
+	std::ofstream model_csv("model.csv");
+	for(int i = 0; i < model_progress.size(); i++) {
+		model_csv << model_progress[i] << '\n';
 	}
 
 	return 0;
